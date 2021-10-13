b45d202a95c833897afe9df2dbfd2d5ffb6cfb2a
jacketchen <342115615@qq.com>
2021-10-14 00:13:30 +0800

    fix: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
+  // parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   ecmaVersion: 2020,
+  //   sourceType: 'module',
+  // },
   extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
